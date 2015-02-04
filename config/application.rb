e9382a28fb03eb877f6552630e05ae51f79082c5
sukyumKim <ikks06luck@naver.com>
2015-02-04 20:31:00 +0900

    modi

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,7 @@ Bundler.require(*Rails.groups)
 
 module PolymerTutorial
   class Application < Rails::Application
+    config.serve_static_files = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
