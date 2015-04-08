7ebb36e3e77ab1548dd3dbca9c22bd0b44f063f1
sukyumKim <ikks06luck@naver.com>
2015-04-08 15:15:54 +0900

    yt modi

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,7 @@ Bundler.require(*Rails.groups)
 module PolymerTutorial
   class Application < Rails::Application
     config.serve_static_files = true
+    config.active_record.observers = :video_observer
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
